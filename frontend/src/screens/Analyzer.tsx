c720efaa1fac933987cc469ff0782946f3b4c29b
Tim Holm <tim.holm@nitric.io>
2024-09-03 16:30:39 +1000

    add nested routing for disk selector

--- frontend/src/screens/Analyzer.tsx
+++ frontend/src/screens/Analyzer.tsx
@@ -1,13 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 
+const DiskSelector = () => {
+    return (
+        <div className="w-full h-full flex flex-col gap-3 pt-1 flex-auto overflow-visible">
+            Disk Selector
+        </div>
+    );
+}
+
 const Analyzer: React.FunctionComponent = () => {
-  return (
-    <DefaultLayout>
-      <div className="w-full h-full flex flex-col gap-3 pt-1 flex-auto overflow-visible">
-        Analyzer Page
-      </div>
-    </DefaultLayout>
-  );
+    return (
+        <DefaultLayout>
+            <Routes location="/analyzer">
+                <Route path="/" element={<DiskSelector />} />
+            </Routes>
+        </DefaultLayout>
+    );
 };
 
 export default Analyzer;
